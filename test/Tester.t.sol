ae0ce3afb73e6685e16a20063ef22584dee12a6c
ross <92001561+z0r0z@users.noreply.github.com>
2023-09-27 13:06:53 +0000

    ⚡ Fork environs

--- test/Tester.t.sol
+++ test/Tester.t.sol
@@ -7,7 +7,13 @@ import "@forge/Test.sol";
 contract TesterTest is Test {
     Tester immutable tester = new Tester();
 
-    function setUp() public payable {}
+    function setUp() public payable {
+        // vm.createSelectFork(vm.rpcUrl('main')); // Ethereum mainnet fork.
+        // vm.createSelectFork(vm.rpcUrl('base')); // Base OptimismL2 fork.
+        // vm.createSelectFork(vm.rpcUrl('poly')); // Polygon network fork.
+        // vm.createSelectFork(vm.rpcUrl('opti')); // Optimism EthL2 fork.
+        // vm.createSelectFork(vm.rpcUrl('arbi')); // Arbitrum EthL2 fork.
+    }
 
     function testTest() public payable {
         tester.test("ommm");
